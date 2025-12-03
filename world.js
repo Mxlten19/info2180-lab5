dc43a1d90cec3e6abb1841c3635fccd795038545
Mxlten19 <nhutton110@gmail.com>
2025-12-02 21:59:18 -0500

    Added world.js

--- world.js
+++ world.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         const xhr = new XMLHttpRequest();
         
-        let url = 'world.php';
+        let url = '/info2180-lab5/world.php';
         if (country) {
             url += '?country=' + encodeURIComponent(country);
         }
