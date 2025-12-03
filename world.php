816e70a5d236084ff72a57dba6980453aa265ade
Mxlten19 <nhutton110@gmail.com>
2025-12-02 20:37:04 -0500

    Edited world.php

---
+++ world.php
@@ -0,0 +1,24 @@
+<?php
+$host = 'localhost';
+$username = 'lab5_user';
+$password = 'password123';
+$dbname = 'world';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+$country = isset($_GET['country']) ? $_GET['country'] : '';
+
+if (!empty($country)) {
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+} else {
+    $stmt = $conn->query("SELECT * FROM countries");
+}
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<ul>
+<?php foreach ($results as $row): ?>
+  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<?php endforeach; ?>
+</ul>
